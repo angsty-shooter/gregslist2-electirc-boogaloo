bbd5cf84cffd4c4f1569e56d68143cfa868a5ac2
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-10 15:09:31 -0700

    car service

---
+++ src/components/Cars.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col-4">
+    <router-link :to="{name: 'CarDetails', params: {id: car._id} }">
+      <div class="card car">
+        <img class="card-img-top" :src="car.imgUrl" alt="car">
+        <div class="card-body">
+          <h4 class="card-title">{{car.make}} | {{car.model}}</h4>
+          <p calss="card-text">{{car.year}} | {{car.price}}</p>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'car',
+  props: {
+    car: Object
+  },
+  setup () {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style scoped>
+.car:hover {
+  transform: translateY(-5px);
+  box-shadow: 1px 3px 5px rgb(94, 94, 94);
+  cursor: pointer
+}
+.car {
+  transition: all .2s;
+  color: black
+}
+</style>
--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -1,3 +1,45 @@
-import { AppState } from '../Appstate'
-import { sandboxApi } from './AxiosService'
+import { AppState } from '../AppState.js'
+import { sandboxApi } from './AxiosService.js'
 
+class CarsService {
+  async getCars () {
+    try {
+      const res = await sandboxApi.get('cars')
+      AppState.cars = res.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getCarId (id) {
+    try {
+      const res = await sandboxApi.get('cars/' + id)
+      AppState.activeCar = res.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async createCar (rawCar) {
+    try {
+      const res = await sandboxApi.post('cars', rawCar)
+      AppState.cars.push(res.data)
+      return res.data._id
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // async bid
+
+  async deleteCar (id) {
+    try {
+      await sandboxApi.delete(`cars/${id}`)
+      this.getCars()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export const carsService = new CarsService()
